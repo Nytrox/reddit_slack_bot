1fc4f56bd1f374580eddb7c48ed5bda094b23f94
Nytrox <neonnytrox@gmail.com>
2019-01-01 14:48:02 -0800

    Implement reddit and slack files

---
+++ reddit.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+reddit_slack_bot.reddit
+~~~~~~~~~~~~
+This module handles reddit auth and API commands
+:copyright: (c) 2018 by Henry Gan.
+"""
+
+def auth(username, password):
+    pass
+
+def register_subreddit(subreddit):
+    pass
